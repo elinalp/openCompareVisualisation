d2df47090b2ffbc2074af79e51b8be534e045b7e
Ornithologue-Elina <elina.lepetit@gmail.com>
2016-11-25 10:46:48 +0100

    Ajout de la javadoc pour FeatureViz et ajout de la classe FeatureVizFactory

--- src/main/java/org/opencompare/FeatureVizFactory.java
+++ src/main/java/org/opencompare/FeatureVizFactory.java
@@ -1,7 +1,13 @@
 package org.opencompare;
 
+import org.opencompare.api.java.Feature;
+
 /**
  * Created by Pierre-Louis on 18/11/2016.
  */
 public class FeatureVizFactory {
+
+    public FeatureViz makeFeatureViz (Feature f){
+        return new FeatureViz(f);
+    }
 }
--- src/test/java/org/opencompare/FeatureVizTest.java
+++ src/test/java/org/opencompare/FeatureVizTest.java
@@ -1,12 +1,10 @@
 package org.opencompare;
 
 import org.junit.Test;
-import org.opencompare.api.java.*;
 import org.opencompare.api.java.Feature;
 import org.opencompare.api.java.PCM;
 import org.opencompare.api.java.impl.io.KMFJSONLoader;
 import org.opencompare.api.java.io.PCMLoader;
-import scala.Int;
 
 import java.io.File;
 
@@ -25,7 +23,7 @@ public class FeatureVizTest {
     public void testGetTypesFeature() throws IOException {
 
         // Load a PCM
-        File pcmFile = new File("pcms/Comparison_of_brainwave_entrainment_software_0.pcm");
+        File pcmFile = new File("pcms/Comparison_of_English_dictionaries_0.pcm");
 
         // Create a loader that can handle the file format
         PCMLoader loader = new KMFJSONLoader();
@@ -35,16 +33,25 @@ public class FeatureVizTest {
 
         // Browse the features of the PCM
         this.listFeature = pcm.getConcreteFeatures();
+
         // Get the seconde feature
-        Feature f = listFeature.get(1);
+        Feature f = listFeature.get(7);
 
         FeatureViz featureViz = new FeatureViz(f);
         System.out.println(featureViz.getCountModalite());
 
-        Hashtable<Class<Value>, Integer> list = featureViz.getTypesFeature();
+        //Hashtable<Class<Value>, Integer> list = featureViz.getTypesFeature();
 
-        System.out.print(list.toString());
+        //System.out.print(list.toString());
+    }
 
+    @Test
+    public void testGetCountModalite() throws IOException {
+
+    }
+
+    @Test
+    public void testGetListCharts() throws IOException {
 
     }
 
