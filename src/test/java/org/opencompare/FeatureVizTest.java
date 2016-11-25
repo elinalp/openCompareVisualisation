0dd0aaae37d4b1801c26c4ccdd159e12b395e591
Ornithologue-Elina <elina.lepetit@gmail.com>
2016-11-25 13:38:04 +0100

    Modification de la classe de test FeatureVizTest

--- src/test/java/org/opencompare/FeatureVizTest.java
+++ src/test/java/org/opencompare/FeatureVizTest.java
@@ -1,52 +1,86 @@
 package org.opencompare;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opencompare.api.java.Feature;
 import org.opencompare.api.java.PCM;
+import org.opencompare.api.java.Value;
 import org.opencompare.api.java.impl.io.KMFJSONLoader;
 import org.opencompare.api.java.io.PCMLoader;
 
 import java.io.File;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by gbecan on 02/02/15.
  */
 public class FeatureVizTest {
 
-    private List<Feature> listFeature;
+    /**
+     * Liste de PCM
+     */
+    private static HashMap<String, PCM> listPcm = new HashMap<>();
 
-    @Test
-    public void testGetTypesFeature() throws IOException {
-
-        // Load a PCM
-        File pcmFile = new File("pcms/Comparison_of_English_dictionaries_0.pcm");
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // Load a PCMS
+        File pcmFile1 = new File("pcms/pcms_test_junit/Comparison_of_English_dictionaries_0.pcm");
+        File pcmFile2 = new File("pcms/Comparison_of_English_dictionaries_0.pcm");
 
         // Create a loader that can handle the file format
         PCMLoader loader = new KMFJSONLoader();
 
         // Get the first PCM
-        PCM pcm = loader.load(pcmFile).get(0).getPcm();
+        PCM pcm1 = loader.load(pcmFile1).get(0).getPcm();
+        listPcm.put("pcm1",pcm1);
+    }
+
+
+    @Test
+    public void testGetTypesFeature() throws IOException {
 
         // Browse the features of the PCM
-        this.listFeature = pcm.getConcreteFeatures();
+        List<Feature> listFeature = listPcm.get("pcm1").getConcreteFeatures();
 
         // Get the seconde feature
-        Feature f = listFeature.get(7);
-
+        Feature f = listFeature.get(8);
         FeatureViz featureViz = new FeatureViz(f);
-        System.out.println(featureViz.getCountModalite());
 
-        //Hashtable<Class<Value>, Integer> list = featureViz.getTypesFeature();
+        Hashtable<Class<Value>, Integer> listeFeature = featureViz.getTypesFeature();
 
-        //System.out.print(list.toString());
+
     }
 
     @Test
     public void testGetCountModalite() throws IOException {
+
+        Feature f;
+        int nbModalite;
+        FeatureViz featureViz;
+
+        // ----  TEST SUR LA PCM1 - English_dictionaries_0
+
+        // Browse the features of the PCM
+        List<Feature> listFeature = listPcm.get("pcm1").getConcreteFeatures();
+        // Get the last feature - Pronunciation guide (String)
+        f = listFeature.get(8);
+        featureViz = new FeatureViz(f);
+        nbModalite = featureViz.getCountModalite();
+
+        assertEquals("Pcm English_dictionaries_0 - Nombre de modalité de la feature " + f.getName(), 2, nbModalite);
+
+        // Get the feature - Year (Integer)
+        f = listFeature.get(4);
+        featureViz = new FeatureViz(f);
+        nbModalite = featureViz.getCountModalite();
+
+        assertEquals("Pcm English_dictionaries_0 - Nombre de modalité de la feature " + f.getName(), 4, nbModalite);
 
     }
 
